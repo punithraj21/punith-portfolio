3f09ec42b9b4fa11501f7fb38a832b8adfaeba15
punithraj21 <punithraj.tcs21@gmail.com>
2023-12-04 00:57:08 +0530

    Added Header

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,48 @@
+import Link from "next/link";
+
+import {
+  RiInstagramLine,
+  RiFacebookLine,
+  RiGithubLine,
+  RiTwitterLine,
+  RiLinkedinLine,
+} from "react-icons/ri";
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="flex items-center gap-x-5 text-lg ">
+      <Link
+        href={"https://www.linkedin.com/in/punith-raj-t"}
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiLinkedinLine />
+      </Link>
+      <Link
+        href={"https://github.com/punithraj21"}
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiGithubLine />
+      </Link>
+      <Link
+        href={"https://www.instagram.com/punith_raj_t"}
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiInstagramLine />
+      </Link>
+      <Link
+        href={"https://www.facebook.com/profile.php?id=100010451714385"}
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiFacebookLine />
+      </Link>
+      <Link
+        href={"https://twitter.com/PunithRajT21"}
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiTwitterLine />
+      </Link>
+    </div>
+  );
 };
 
 export default Socials;
--- pages/index.js
+++ pages/index.js
@@ -29,8 +29,8 @@ const Home = () => {
             exit="hidden"
             className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"
           >
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi
-            non quis exercitationem culpa nesciunt nihil aut nostrum explicabo
+            Diligent software engineer with stong experience in Web development
+            and Android development.
           </motion.p>
           <div className="flex justify-center xl:hidden relative">
             <ProjectBtn />
